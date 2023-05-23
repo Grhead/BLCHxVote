3590ae9be06702641852f55833709444626cb833
Grenka <64643423+Grhead@users.noreply.github.com>
2023-05-23 19:02:12 +0700

    fix

--- Main.go
+++ Main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"VOX2/Blockchain"
-	"fmt"
-)
-
-//type BlockHelp struct {
-//	Block   *Blockchain.Block `form:"block" json:"block"`
-//	Size    uint64            `form:"size" json:"size"`
-//	Address string            `form:"address" json:"address"`
-//}
-//type re struct {
-//	AddTxStatus string
-//}
-
 func main() {
-	db, err := Blockchain.GetFullDb()
-	if err != nil {
-		return
-	}
-	for _, v := range db {
-		fmt.Println(v)
-	}
+
 }
